7e94e1104f102296ee3990532fbeb6f2aee2cc76
2gingersnaps <robbikenney@gmail.com>
2018-03-31 15:09:46 -0400

    Default values for Review model

--- models/Review.js
+++ models/Review.js
@@ -1,11 +1,26 @@
 
 const mongoose = require('mongoose');
+var defaultUserID = function() {
+  return "10101010";
+};
+var defaultTimestamp = function() {
+  return Date.now
+};
+var defaultProductName = function() {
+  return "Product 000"
+};
+var defaultComment = function() {
+  return "NO COMMENT"
+};
+var defaultRating = function() {
+  return "0"
+};
 const ReviewSchema = new mongoose.Schema({
-  userID: String, // generate a uuid if no real userID is present
-  timestamp: { type : Date, default: Date.now },
-  productName: String, // name of product being rated
-  comment: String, // comment from User
-  rating: String
+  userID: { type: String, default: defaultUserID() }, // generate a uuid if no real userID is present
+  timestamp: { type : Date, default: defaultTimestamp() },
+  productName: { type: String, default: defaultProductName() },
+  comment: { type: String, default: defaultComment() },
+  rating: { type: String, default: defaultRating() }
   // geolocation
 });
 module.exports = mongoose.model('Review', ReviewSchema);
