160b7fedd7de126763cad4acbfb4334568d6a81c
2gingersnaps <robbikenney@gmail.com>
2018-04-01 14:19:52 -0400

    Add Sample custom query to get reviews by userID and rating

--- handler.js
+++ handler.js
@@ -95,3 +95,22 @@ module.exports.delete = (event, context, callback) => {
         }));
     });
 };
+
+// sample custom query to get all reviews by userID and rating
+module.exports.report = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Review.find({ 'userID': event.pathParameters.userID, 'rating': event.pathParameters.rating  })
+        .then(reviews => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(reviews)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the report.'
+        }))
+    });
+};
